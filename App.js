e48fc2d430fd4f03757d2e46f655e50812446dde
Osurian <97887149+Osurian@users.noreply.github.com>
2022-06-02 09:52:56 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,287 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <body className='body'>
+
+        <h1 id="cardapio-titulo">Cardapio</h1>
+        <button><h1 id="topico">Entradaaaaaas</h1></button>
+        <button><h1 id="topico">Entradas</h1></button>
+            <div id="cardapio">
+
+                
+                    <div id="item-cardapio">
+                    <h2> E01-Croquete de Bacalhau (10 unidades)   </h2>
+                        
+                        <div id="card-description">
+
+                         Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:40,00 </h2>
+                    </div>
+                  
+                    <div id="item-cardapio">
+                        <h2> E03-Cesto de Salada de Parmesão com Mix de Folhas </h2>
+                        <div id="card-description">
+                            Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:30,00 </h2>
+                    </div> 
+                    <div id="item-cardapio">
+                        <h2>E05-Tábua Adega </h2>
+                     
+                        <div id="card-description">
+                            (queijo, azeitona, salame e torradinhas) Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:40,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>E07-Tábua Provolera  </h2>
+                       
+                        <div id="card-description">
+                            (provolone na pedra com orégano) Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:32,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>E09-Trio de Bruschettas  </h2>
+                      
+                        <div id="card-description">
+                            (alho poró – cogumelos – tomates cereja)Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:35,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>E011-Chapa Adega  </h2>
+                       
+                        <div id="card-description">
+                            (picanha grelhada, tomatecereja e queijo provolone)Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:110,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>E013-Bolinho de Carne seca  </h2>
+                      
+                        <div id="card-description">
+                            (09-unidades)Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:32,00 </h2>
+                    </div>
+                    
+                    
+                
+                
+                    
+                    <div id="item-cardapio">
+                        <h2>E02- Burrata </h2>
+                       
+                        <div id="card-description">
+                            (quejo de búfala torradas e tomates conflitados) Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:52,00 </h2>
+                    </div> 
+                    <div id="item-cardapio">
+                        <h2> E04-Salada Italiana com cubos de frango </h2>
+                        
+                        <div id="card-description">
+                            Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:28,00 </h2>
+                    </div>    
+                    <div id="item-cardapio">
+                        <h2>E06-Tábua Quatro Queijos Adega </h2>
+                      
+                        <div id="card-description">
+                            (provolone, nozinho, parmesão e gouda) Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:50,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>E08-Salada Caprese  </h2>
+               
+                        <div id="card-description">
+                            (tomates-cereja ao azeite, manjericão e queijo bolinha)Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:30,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>E010-Brie com Mel  </h2>
+                      
+                        <div id="card-description">
+                            (queijo brie, mel, castanhas e torradinhas)Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:58,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>E012-Ostras Gratinadas  </h2>
+                     
+                        <div id="card-description">
+                            (08-unidades)Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:66,00 </h2>
+                    </div>
+                </div>
+                
+                
+            
+            <h1 id="topico">pratos</h1>
+
+            <div id="cardapio">
+
+                <div id="totem">
+                    <div id="item-cardapio">
+                        <h2>P01-Prime Rib com Risoto de Brócolis e Tomates  </h2>
+                        
+                        <div id="card-description">
+                            Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:102,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P03-Lasanha de Carne Desfiada </h2>
+                       
+                        <div id="card-description">
+                            Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:68,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P05-Fettuccine Al Mare </h2>
+                      
+                        <div id="card-description">
+                            (lula, camarão, mariscos e azeite provençal) Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:88,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P07-Fettuccine Alla Vodca </h2>
+                      
+                        <div id="card-description">
+                            (bacon)Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:56,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P09-Risotto al porro e mignon na mostarda  dijon </h2>
+                       
+                        <div id="card-description">
+                            Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:65,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P11-Risotto de Maça Verde com Carré de Cordeiro Grelhado </h2>
+                      
+                        <div id="card-description">
+                            (abobrinha) Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:80,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P13-Risotto de Gorgonzola e Mignon Grelhado </h2>
+                        
+                        <div id="card-description">
+                             Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:68,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P15-Ravióli de Ricota e Cogumelo  </h2>
+                        
+                        <div id="card-description">
+                            (Vinho branco e bacon)Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:68,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P17-Prato Kids    </h2>
+                       
+                        <div id="card-description">
+                            (Penne, mignon em cubos e fritas)Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:45,00 </h2>
+                    </div>
+                    
+                </div>
+                <div id="toten">
+                    
+                    <div id="item-cardapio">
+                        <h2>P02-Fettuccine com Tomates, Manjericão, Mignon Grelhado ao Molho Gorgonzola </h2>
+                      
+                        <div id="card-description">
+                            Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:99,00 </h2>
+                    </div> 
+                    <div id="item-cardapio">
+                        <h2>P04-Lasanha Parisiense </h2>
+                       
+                        <div id="card-description">
+                            (Frango, presunto, ervilha, molho bechamel e mussarela) Serve 2P.
+                        </div>
+                        <h2 class="valor">R$:68,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P06-Spaghetti ai Pomodori Secchi  </h2>
+                        
+                        <div id="card-description">
+                            (massa com tomate seco rúcula e chorizo com fonduta de queijo) Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:60,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P08-Spaghetti aglio e oli e broccoli </h2>
+                        
+                        <div id="card-description">
+                            (acompanha peito de frango grelhado com ervas)Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:56,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P10-Risotto Zucchini e Parmesão  </h2>
+                        
+                        <div id="card-description">
+                            (abobrinha) Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:65,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P12-Risotto al Limone Siciliano com Salmão Grelhado </h2>
+                     
+                        <div id="card-description">
+                             Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:75,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P14-Risotto al Parmigiano com Polpetone </h2>
+                        
+                        <div id="card-description">
+                             Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:68,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P16-Risotto Mare e Monti   </h2>
+                       
+                        <div id="card-description">
+                            (bacon, camarão, champignon e vinho branco)Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:75,00 </h2>
+                    </div>
+                    <div id="item-cardapio">
+                        <h2>P18-Cotolleta alla Milanese     </h2>
+                        
+                        <div id="card-description">
+                            (Carré suíno à milanesa e risoto ao pomodoro)Serve 1P.
+                        </div>
+                        <h2 class="valor">R$:68,00 </h2>
+                    </div>
+                </div>
+            </div>
+    </body>
+  );
+}
+
+export default App;
